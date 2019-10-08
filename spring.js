ef239391d1933e872b54eea5cbbcd0d5ea6848bd
AnnOrl <30530193+AnnOrl@users.noreply.github.com>
2019-10-08 18:24:17 +0300

    Update spring.js

--- spring.js
+++ spring.js
@@ -29,29 +29,33 @@ function SpringSolver(mass, stiffness, damping, initialVelocity) {
 }
 
 const start = function(params) {
-    const {startTime, solver, fraction, from, to, maxPeriod, values: valuesP, finite, index} = params;
+    const {startTime, solver, fraction, from, to, maxPeriod, values: valuesP, finite, index, prevValue} = params;
     let values = valuesP;
     const elapsed = Date.now() / 1000 - startTime;
     const proportion = solver(elapsed);
-
     let current = from + (to - from) * proportion;
 
     if (fraction) {
         current = Math.round(current * 10 ** fraction) / 10 ** fraction;
     }
+
     //ограничение в периоде колебаний
     if (finite < maxPeriod) {
         try {
+            const isUnic = prevValue === false || prevValue !== current;
+
             values = {
                 ...values,
                 ...start({
                     ...params,
                     finite: current === to ? finite + 1 : finite,
-                    index: index + 1,
-                    values: {
-                        ...values,
-                        [index]: current,
-                    },
+                    index: isUnic ? index + 1 : index,
+                    prevValue: isUnic ? current : index,
+                    values: isUnic ? {
+                            ...values,
+                            [index]: current,
+                        }
+                        : values,
                 }),
             };
         } catch (e) {
@@ -80,18 +84,21 @@ const spring = (params = {}) => {
         finite: 0,
         index: 0,
         values: {},
+        prevValue: false,
     };
     pr.startTime = Date.now() / 1000;
     pr.solver = SpringSolver(pr.mass, pr.stiffness, pr.damping, pr.initialVelocity);
 
-    return start(pr);
+    const values = start(pr);
+
+    return values;
 };
 
 const getMainFrame = (params, values, onlyValue) => {
     const stepsKeys = Object.keys(values);
     const step = Math.round(stepsKeys.length / 100);
 
-    return (i) => onlyValue ? values[i * step] : formatName({...params, value: values[i * step]});
+    return (i) => (onlyValue ? values[i * step] : formatName({...params, value: values[i * step]}));
 };
 const springs = (animations, params = {}) => {
     let fromFrame, toFrame, mainFrame;
@@ -100,26 +107,37 @@ const springs = (animations, params = {}) => {
     if (Array.isArray(animations)) {
         const values = animations.map((param) => spring({...params, ...param}));
 
-        fromFrame = animations.map((param) => {
-            let animation = {...param, ...params};
+        fromFrame = animations
+            .map((param) => {
+                let animation = {...param, ...params};
 
-            return params.name ? animation.from : formatName({
-                ...animation,
-                value: animation.from,
-            });
-        }).join(params.name ? ',' : ';');
-
-        toFrame = animations.map((param) => {
-            let animation = {...param, ...params};
-            return params.name ? animation.to : formatName({
-                ...animation,
-                value: animation.to,
-            });
-        }).join(params.name ? ',' : ';');
-
-        const valuesFrame = (i) => values.map((value, key) =>
-            getMainFrame({...params, ...animations[key]}, value, !!params.name)(i))
+                return params.name
+                    ? animation.from + (param.postfix || '')
+                    : formatName({
+                        ...animation,
+                        value: animation.from,
+                    });
+            })
             .join(params.name ? ',' : ';');
+
+        toFrame = animations
+            .map((param) => {
+                let animation = {...param, ...params};
+                return params.name
+                    ? animation.to + (param.postfix || '')
+                    : formatName({
+                        ...animation,
+                        value: animation.to,
+                    });
+            })
+            .join(params.name ? ',' : ';');
+
+        const valuesFrame = (i) =>
+            values
+                .map(
+                    (value, key) => getMainFrame({...params, ...animations[key]}, value, !!params.name)(i) + (animations[key].postfix || ''),
+                )
+                .join(params.name ? ',' : ';');
 
         mainFrame = valuesFrame;
 
@@ -137,25 +155,23 @@ const springs = (animations, params = {}) => {
     }
     let keyframes = `{\n from {\n${fromFrame}\n}\n`;
     for (let i = 1; i < 100; i++) {
-        keyframes = i % animation.fraction === 0 ? keyframes +
-            `${i}% {\n${mainFrame(i)}\n}\n` : keyframes;
+        keyframes = i % animation.step === 0 ? keyframes + `${i}% {\n${mainFrame(i)}\n}\n` : keyframes;
     }
     return keyframes + `to {\n${toFrame}\n}\n}`;
 };
 
 console.log(
-    springs([{
-        stiffness: 381.47,
-        damping: 15.17,
-    }, {
-        stiffness: 381.47,
-        damping: 20.17,
-    }], {
-        from: 1.085,
-        to: 1,
-        name: 'transform',
-        subName: 'scale',
-        maxPeriod: 3,
-        fraction: 5,
-    }),
+    springs(
+        {
+            from: 1.085,
+            to: 1,
+            stiffness: 381.47,
+            damping: 15.17,
+            name: 'transform',
+            subName: 'scale',
+            maxPeriod: 5,
+            fraction: 5,
+            step: 10,
+        },
+    ),
 );
